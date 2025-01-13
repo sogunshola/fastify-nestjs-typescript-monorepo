8c892f0c73fd139d3df160e6705367a868ea813f
Shalom Ogunshola <shalomogunshola@gmail.com>
2025-01-13 11:22:04 +0100

    namespace fix

--- packages/shared/src/plugins/cloud-storage/IStorage.ts
+++ packages/shared/src/plugins/cloud-storage/IStorage.ts
@@ -2,7 +2,7 @@ export interface IStorage {
   getFileUrl(filename: string): Promise<string>;
   uploadFile(
     filenameOrPath: string,
-    body?: Buffer | Express.Multer.File,
+    body?: Buffer | any,
     options?: Record<string, unknown>,
   ): Promise<string>;
   downloadFile(filename: string): Promise<Buffer>;
